751850d33a22417ae0443c8742e272dd256f663c
mjc23 <matt.clarke@stfc.ac.uk>
2017-07-10 13:06:52 +0100

    Added data from book for sanity checking

--- data_miner.py
+++ data_miner.py
@@ -1,3 +1,61 @@
+import re
+
+
 class DataMiner(object):
     def extract_number_commits(self, raw_data):
-        pass
+        """
+        Returns the number of commits contained in the data.
+
+        Used with prettified output from:
+            git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat
+
+        :param raw_data: the output of the git log
+        :return: number of commits in the output
+        """
+        num_commits = 0
+        lines = raw_data.strip().split('\n')
+        for line in lines:
+            if line.strip().startswith("["):
+                num_commits += 1
+        return num_commits
+
+    def extract_number_authors(self, raw_data):
+        """
+        Returns the number of authors who have committed commits.
+
+        Used with prettified output from:
+            git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat
+
+        :param raw_data: the output of the git log
+        :return: number of committing authors
+        """
+        authors = set()
+        lines = raw_data.strip().split('\n')
+        for line in lines:
+            # Something like: [cef94a2] mjc23 2017-07-10 Added DataMiner class
+            m = re.match(r"\s*\[\w*\]\s(\S*)\s.*", line)
+            if m is not None:
+                authors.add(m.groups()[0])
+        return len(authors)
+
+    def extract_number_entities_changed(self, raw_data):
+        """
+        Returns the number of times file have been changed.
+
+        Used with prettified output from:
+            git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat
+
+        :param raw_data: the output of the git log
+        :return: number of files changed
+        """
+        count = 0
+        lines = raw_data.strip().split('\n')
+        for line in lines:
+            # Something like:
+            # 3       0       data_miner.py
+            # or:
+            # -       -       some.png
+            m = re.match(r"\s*[\d|-].*", line)
+            if m is not None:
+                count += 1
+        return count
--- tests/data_mining_tests.py
+++ tests/data_mining_tests.py
@@ -1,25 +1,53 @@
 import unittest
 from data_miner import DataMiner
 
+GITLOG_DATA = """
+[f9f97a0] Matt Clarke 2017-07-10 Added png for testing purposes
+-       -       tests/example.png
+
+[cef94a2] mjc23 2017-07-10 Added DataMiner class
+3       0       data_miner.py
+0       12      tests/DataMiningTests.py
+25      0       tests/data_mining_tests.py
+
+[74f6bbf] mjc23 2017-07-10 Added first unit test
+12      0       tests/data_mining_tests.py
+
+[1dbf879] mjclarke01 2017-07-10 Create README.md
+2       0       README.md
+
+[9d18261] mjclarke01 2017-07-10 Initial commit
+101     0       .gitignore
+29      0       LICENSE
+"""
+
 
 class DataMiningGitLogTests(unittest.TestCase):
-    def correctly_extracts_number_of_commits_from_git_log(self):
-        """
-        Used with output from:
-            git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat
-        """
-        raw_data = """
-        [74f6bbf] mjc23 2017-07-10 Added first unit test
-        12      0       tests/data_mining_tests.py
+    def test_correctly_extracts_number_of_commits_from_git_log(self):
+        data_miner = DataMiner()
+        self.assertEqual(5, data_miner.extract_number_commits(GITLOG_DATA))
 
-        [1dbf879] mjclarke01 2017-07-10 Create README.md
-        2       0       README.md
+    def test_if_commit_id_in_commit_message_number_of_commits_still_correct(self):
+        # Stuff a commit id into a commit message - it might happen in real life
+        modified_data = GITLOG_DATA.replace("Create README.md", "Create README.md [cef94a2]")
+        data_miner = DataMiner()
+        self.assertEqual(5, data_miner.extract_number_commits(modified_data))
 
-        [9d18261] mjclarke01 2017-07-10 Initial commit
-        101     0       .gitignore
-        29      0       LICENSE
-        """
+    def test_correctly_extracts_number_of_authors(self):
+        data_miner = DataMiner()
+        self.assertEqual(3, data_miner.extract_number_authors(GITLOG_DATA))
+
+    def test_correctly_extracts_number_of_entities_changed(self):
+        data_miner = DataMiner()
+        self.assertEqual(8, data_miner.extract_number_entities_changed(GITLOG_DATA))
+
+    def test_data_miner_using_code_maat_data(self):
+        maat_data = ""
+        with open("maat_evo.log") as file:
+            maat_data = file.read()
 
         data_miner = DataMiner()
-        num = data_miner.extract_number_commits(raw_data)
-        self.assertEqual(3, num)
+        self.assertEqual(88, data_miner.extract_number_commits(maat_data))
+        # TODO: Fails because of the space in the name
+        self.assertEqual(2, data_miner.extract_number_authors(maat_data))
+        self.assertEqual(283, data_miner.extract_number_entities_changed(maat_data))
