1edf1d18b9b07028ee9cb12094642c804ee8926a
Matt Clarke <matt.clarke@esss.se>
2025-12-05 12:08:15 +0100

    Update hotspot_compiler.py

--- src/hotspot_compiler.py
+++ src/hotspot_compiler.py
@@ -1,35 +1,58 @@
-from hotspot_compiler import merge_raw_data, remove_files_with_no_code
+from src.weights_normaliser import normalise_weights
 
 
-def test_given_number_of_commit_and_lines_count_then_ordered_by_highest_number_of_commits():
-    commits = {"File1": 5, "File2": 15, "File3": 10}
-    loc = {"File1": 123, "File2": 321, "File3": 1000}
-    ans = merge_raw_data(commits, loc)
-    assert "File2" == ans[0][0]
-    assert "File3" == ans[1][0]
-    assert "File1" == ans[2][0]
+def create_hotspots(commits, loc):
+    """
+    Creates the hotspots.
 
+    :param commits: a dict of file names and the number of commits
+    :param lines_of_code: a dict of file names and the number of lines of code
+    :return: a sorted list of tuple (name, changes, lines)
+    """
 
-def test_given_number_of_commits_equal_then_ordered_by_highest_number_of_lines():
-    commits = {"File1": 5, "File2": 5, "File3": 5}
-    loc = {"File1": 123, "File2": 321, "File3": 1000}
-    ans = merge_raw_data(commits, loc)
-    assert "File3" == ans[0][0]
-    assert "File2" == ans[1][0]
-    assert "File1" == ans[2][0]
-
-
-def test_files_with_no_code_are_removed():
-    commits = {"File1": 5, "File2": 5, "File3": 5}
-    loc = {"File1": 123, "File2": 0, "File3": 1000}
+    # Remove any files with no lines of code before normalisation
     remove_files_with_no_code(commits, loc)
-    assert 2 == len(commits)
-    assert "File2" not in commits
+
+    # Normalise the data into weights
+    weights = normalise_weights(commits)
+
+    # Compile hotspots
+    return merge_raw_data(weights, loc)
 
 
-def test_files_that_no_longer_exist_are_removed():
-    commits = {"File1": 5, "File2": 5, "File3": 5}
-    loc = {"File1": 123, "File3": 1000}
-    remove_files_with_no_code(commits, loc)
-    assert 2 == len(commits)
-    assert "File2" not in commits
+def remove_files_with_no_code(commits, lines_of_code):
+    """
+    Removes files that either have no lines of code or no longer exist but are
+    historically present in the commits list.
+
+    :param commits: a dict of file names and the number of commits
+    :param lines_of_code: a dict of file names and the number of lines of code
+    """
+    for k in list(commits.keys()):
+        if k not in lines_of_code or lines_of_code[k] == 0:
+            del commits[k]
+
+
+def merge_raw_data(commits, lines_of_code):
+    """
+    Creates a list of the files sorted primarily by the number of changes
+    and secondly by the number of lines of code.
+
+    :param commits: a dict of file names and the number of commits
+    :param lines_of_code: a dict of file names and the number of lines of code
+    :return: a sorted list of tuple (name, changes, lines)
+    """
+    if not commits:
+        return []
+
+    data = []
+    for k, v in commits.items():
+        # File may not exist anymore but is still in log
+        # Skip these files
+        if k in lines_of_code:
+            data.append((k, v, lines_of_code[k]))
+    # Sort on secondary key
+    data = sorted(data, key=lambda x: x[2], reverse=True)
+    # Sort on primary key as this gives sorted by first value then by
+    # second value
+    return sorted(data, key=lambda x: x[1], reverse=True)
