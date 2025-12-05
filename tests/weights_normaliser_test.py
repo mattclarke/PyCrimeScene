44d12aff5be6154eac089ee3e308708e9db145a7
Matt Clarke <matt.clarke@esss.se>
2025-12-05 11:41:58 +0100

    Update weights_normaliser_test.py

--- tests/weights_normaliser_test.py
+++ tests/weights_normaliser_test.py
@@ -1,22 +1,25 @@
 from weights_normaliser import normalise_weights
 
 
-def test_given_one_weight_then_weight_normalised_to_one(self):
+def test_given_one_weight_then_weight_normalised_to_one():
     commits = {"File1": 5}
     weights = normalise_weights(commits)
-    self.assertEqual(1.0, weights["File1"])
+
+    assert weights["File1"] == 1.0
 
 
-def test_given_two_weights_then_both_weights_normalised(self):
+def test_given_two_weights_then_both_weights_normalised():
     commits = {"File1": 5, "File2": 10}
     weights = normalise_weights(commits)
-    self.assertEqual(0.5, weights["File1"])
-    self.assertEqual(1.0, weights["File2"])
+
+    assert weights["File1"] == 0.5
+    assert weights["File2"] == 1.0
 
 
-def test_given_three_weights_then_all_weights_normalised(self):
+def test_given_three_weights_then_all_weights_normalised():
     commits = {"File1": 5, "File2": 10, "File3": 20}
     weights = normalise_weights(commits)
-    self.assertEqual(0.25, weights["File1"])
-    self.assertEqual(0.5, weights["File2"])
-    self.assertEqual(1.0, weights["File3"])
+
+    assert weights["File1"] == 0.25
+    assert weights["File2"] == 0.5
+    assert weights["File3"] == 1.0
