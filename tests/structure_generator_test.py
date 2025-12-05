f0d9bff76e5a47f147b0ddc3c9d1799d849978e1
Matt Clarke <matt.clarke@esss.se>
2025-12-05 11:20:11 +0100

    Update structure_generator_test.py

--- tests/structure_generator_test.py
+++ tests/structure_generator_test.py
@@ -1,79 +1,77 @@
-import unittest
 from structure_generator import generate_structure
 
 
-class GenerateStructureTests(unittest.TestCase):
-    def setUp(self):
-        pass
+def test_given_one_file_get_simple_structure():
+    files = [("test/src/file.java", 1.0, 10)]
+    top_node = generate_structure(files)
 
-    def test_given_one_file_get_simple_structure(self):
-        files = [("test/src/file.java", 1.0, 10)]
-        top_node = generate_structure(files)
+    assert "root" == top_node["name"]
+    child = top_node["children"][0]
+    assert "test" == child["name"]
+    child = child["children"][0]
+    assert "src" == child["name"]
+    child = child["children"][0]
+    assert "file.java" == child["name"]
 
-        self.assertEqual("root", top_node["name"])
-        child = top_node["children"][0]
-        self.assertEqual("test", child["name"])
-        child = child["children"][0]
-        self.assertEqual("src", child["name"])
-        child = child["children"][0]
-        self.assertEqual("file.java", child["name"])
 
-    def test_given_two_file_in_different_directories(self):
-        files = [("test/src/file.java", 1.0, 10), ("doc/rst/readme.txt", 1.0, 10)]
-        top_node = generate_structure(files)
+def test_given_two_file_in_different_directories():
+    files = [("test/src/file.java", 1.0, 10), ("doc/rst/readme.txt", 1.0, 10)]
+    top_node = generate_structure(files)
 
-        self.assertEqual("root", top_node["name"])
+    assert "root" == top_node["name"]
 
-        # First file
-        child = top_node["children"][0]
-        self.assertEqual("test", child["name"])
-        child = child["children"][0]
-        self.assertEqual("src", child["name"])
-        child = child["children"][0]
-        self.assertEqual("file.java", child["name"])
+    # First file
+    child = top_node["children"][0]
+    assert "test" == child["name"]
+    child = child["children"][0]
+    assert "src" == child["name"]
+    child = child["children"][0]
+    assert "file.java" == child["name"]
 
-        # Second file
-        child = top_node["children"][1]
-        self.assertEqual("doc", child["name"])
-        child = child["children"][0]
-        self.assertEqual("rst", child["name"])
-        child = child["children"][0]
-        self.assertEqual("readme.txt", child["name"])
+    # Second file
+    child = top_node["children"][1]
+    assert "doc" == child["name"]
+    child = child["children"][0]
+    assert "rst" == child["name"]
+    child = child["children"][0]
+    assert "readme.txt" == child["name"]
 
-    def test_given_two_file_in_same_directory(self):
-        files = [("test/src/file1.java", 1.0, 10), ("test/src/file2.java", 1.0, 10)]
-        top_node = generate_structure(files)
 
-        self.assertEqual("root", top_node["name"])
+def test_given_two_file_in_same_directory():
+    files = [("test/src/file1.java", 1.0, 10), ("test/src/file2.java", 1.0, 10)]
+    top_node = generate_structure(files)
 
-        child = top_node["children"][0]
-        self.assertEqual("test", child["name"])
-        child = child["children"][0]
-        self.assertEqual("src", child["name"])
-        child1 = child["children"][0]
-        child2 = child["children"][1]
-        # First file
-        self.assertEqual("file1.java", child1["name"])
+    assert "root" == top_node["name"]
 
-        # Second file
-        self.assertEqual("file2.java", child2["name"])
+    child = top_node["children"][0]
+    assert "test" == child["name"]
+    child = child["children"][0]
+    assert "src" == child["name"]
+    child1 = child["children"][0]
+    child2 = child["children"][1]
+    # First file
+    assert "file1.java" == child1["name"]
 
-    def test_given_two_file_in_same_directory_structure(self):
-        files = [("test/src/file1.java", 1.0, 10), ("test/file2.java", 1.0, 10)]
-        top_node = generate_structure(files)
+    # Second file
+    assert "file2.java" == child2["name"]
 
-        self.assertEqual("root", top_node["name"])
 
-        child = top_node["children"][0]
-        self.assertEqual("test", child["name"])
-        child = child["children"][0]
-        self.assertEqual("src", child["name"])
-        child = child["children"][0]
-        # First file
-        self.assertEqual("file1.java", child["name"])
+def test_given_two_file_in_same_directory_structure():
+    files = [("test/src/file1.java", 1.0, 10), ("test/file2.java", 1.0, 10)]
+    top_node = generate_structure(files)
 
-        # Second file
-        child = top_node["children"][0]
-        self.assertEqual("test", child["name"])
-        child = child["children"][1]
-        self.assertEqual("file2.java", child["name"])
+    assert "root" == top_node["name"]
+
+    child = top_node["children"][0]
+    assert "test" == child["name"]
+    child = child["children"][0]
+    assert "src" == child["name"]
+    child = child["children"][0]
+    # First file
+    assert "file1.java" == child["name"]
+
+    # Second file
+    child = top_node["children"][0]
+    assert "test" == child["name"]
+    child = child["children"][1]
+    assert "file2.java" == child["name"]
